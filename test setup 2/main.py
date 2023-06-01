4b3e841797024392717ed7945a5083dd7987cd3d
Carlos-SJS <carlos.sjs@outlook.com>
2023-05-31 18:35:09 -0600

    Update

--- test setup 2/main.py
+++ test setup 2/main.py
@@ -2,6 +2,7 @@ from round_robin import RoundRobin
 from PlayerBot import TTCPlayer
 from TTC_Crusher import TTC_Crusher_Player
 from TuriBase import TuriBase_player
+from Rompe import Rompe
 
 def print_winner():
     with open('results/leaderboard.txt', 'r') as f:
@@ -19,8 +20,9 @@ def print_winner():
         print(f"The winner is: {winner}")
         print(f"{wscore} - {lscore}")
 
+player1 = Rompe("Coca")
 #player1 = TTCPlayer("Kongroo")
-player1 = TuriBase_player("Base bot")
+#player1 = TuriBase_player("Base bot")
 player2 = TTC_Crusher_Player("Gennedy Korotkevich")
 #player3 = TTCPlayer("juan3")
 #player4 = TTCPlayer("juan4")
